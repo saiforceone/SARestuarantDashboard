8f2dd603d89ed1b3a4a102b6f9e89d811f3d9c98
saiforceone <saiforceone@gmail.com>
2022-02-01 20:55:16 -0500

    update api utils to include functionality to create or update a resource as well as delete a resource

--- src/store/actions/actionConstants.js
+++ src/store/actions/actionConstants.js
@@ -5,4 +5,5 @@ export const ERROR_STRINGS = {
 export const ERROR_VALUES = {
   AUTH_FAILED: 'Failed to authenticate',
   INVALID_REQUEST: 'Invalid Request',
+  INVALID_TOKEN: 'Invalid token'
 }
--- src/utils/APIUtils.js
+++ src/utils/APIUtils.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import QueryString from 'qs';
 import { API_ENDPOINTS } from '../constants';
+import { ERROR_STRINGS, ERROR_VALUES } from '../store/actions/actionConstants';
 import StorageUtils, {STORAGE_CONSTANTS} from './StorageUtils';
 
 const STATUS_CODES = {
@@ -24,6 +25,19 @@ class APIUtils {
       statusCode: 400,
       success: false,
     };
+  }
+
+  /**
+   * @method getAuthToken
+   * @returns {string | undefined}
+   * @description Attempts to retrieve the auth token from local storage
+   */
+  static getAuthToken() {
+    let tokenData = StorageUtils.getValueFromStorage({
+      key: STORAGE_CONSTANTS.KEY_AUTH_TOKEN,
+    });
+
+    return tokenData.data;
   }
 
   /**
@@ -79,6 +93,91 @@ class APIUtils {
     } catch (e) {
       response.error = e.toString();
       response.statusCode = STATUS_CODES.BAD_REQUEST;
+      return response;
+    }
+  }
+
+  /**
+   * @static
+   * @method saveOrUpdateResource
+   * @param {string} endpoint
+   * @param {string} id
+   * @param {object} data
+   * @returns {{data: *, error: String, statusCode: Number, success: Boolean}}
+   * @desription Attempts to save a new or update an existing resource
+   */
+  static async saveOrUpdateResource({
+    endpoint = '',
+    id = undefined,
+    data = {},
+  }) {
+    const response = this.buildResponse();
+    try {
+
+      const tokenData = StorageUtils.getValueFromStorage({
+        key: STORAGE_CONSTANTS.KEY_AUTH_TOKEN,
+      });
+
+      if (!tokenData.data) {
+        response.error = `${ERROR_STRINGS.FAILED_WITH_ERROR} ${tokenData.error}`;
+        return response;
+      }
+
+      const headers = {
+        authorization: tokenData.data,
+      };
+
+      let url = `${API_ENDPOINTS.BASE}${endpoint}`;
+
+      if (id) {
+        url += `/${id}`;
+      }
+
+      const {data: responseData} = id 
+        ? await axios.put(url, data, {headers})
+        : await axios.post(url, data, {headers});
+
+      response.error = responseData.error;
+      response.statusCode = response.error ? STATUS_CODES.BAD_REQUEST : STATUS_CODES.POST;
+      response.success = responseData.success;
+      return response;
+    } catch (e) {
+      response.error = e.toString();
+      response.statusCode = STATUS_CODES.SERVER_ERROR;
+      return response;
+    }
+  }
+
+  /**
+   * @method deleteResource
+   * @param {string} endpoint
+   * @param {string} id
+   * @returns {{data: *, error: String, statusCode: Number, success: Boolean}}
+   * @description Given an endpoint and an id, attempts to delete the resource
+   */
+  static async deleteResource({endpoint = '', id}) {
+    const response = this.buildResponse();
+    try {
+      const token = this.getAuthToken();
+      
+      if (!token) {
+        response.error = `${ERROR_VALUES.INVALID_TOKEN}`;
+        return response;
+      }
+
+      const headers = {
+        authorization: token,
+      };
+
+      const {data: responseData} = await axios.delete(url, {headers});
+
+      response.error = responseData.error;
+      response.statusCode = responseData.error ? STATUS_CODES.BAD_REQUEST : STATUS_CODES.OK;
+      response.success = responseData.success;
+      return response;
+    } catch (e) {
+      response.statusCode = STATUS_CODES.SERVER_ERROR;
+      response.error = e.toString();
       return response;
     }
   }
