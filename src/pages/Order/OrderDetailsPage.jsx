24843d4cb0d9bd39e4c53a8f76ebdb6fef361ac2
saiforceone <saiforceone@gmail.com>
2022-02-12 23:40:43 -0500

    update order details screen

--- src/pages/Order/OrderDetailsPage.jsx
+++ src/pages/Order/OrderDetailsPage.jsx
@@ -1,7 +1,49 @@
 import React, {useCallback, useEffect, useState} from 'react';
-import { useParams, useLocation } from 'react-router';
+import { useParams, useLocation, useNavigate } from 'react-router';
+import { Form } from 'react-bootstrap';
+import {CheckCircleFill as CheckIcon} from 'react-bootstrap-icons';
 import APIUtils from '../../utils/APIUtils';
 import { API_ENDPOINTS, ORDER_STATUSES } from '../../constants';
+import PageHeader from '../../components/PageHeader/PageHeader';
+
+/**
+ * @function UpdateStatusControl
+ * @param {String} orderStatus
+ * @returns {JSX.Element}
+ * @description Renders the update status control
+ */
+const UpdateStatusControl = ({
+  orderStatus,
+  onUpdateStatus,
+  updateStatusAction
+}) => {
+  return (
+    <div className='d-flex align-items-center'>
+      <div>
+        <Form.Select
+          onChange={e => onUpdateStatus({orderStatus: e.target.value})}
+          name='order-status'
+          value={orderStatus}
+        >
+          <option value="">Select Status</option>
+          {ORDER_STATUSES.map(status => (
+            <option key={status.value} value={status.value}>
+              {status.label}
+            </option>
+          ))}
+        </Form.Select>
+      </div>
+      <button
+        className='btn btn-primary ms-4'
+        onClick={updateStatusAction}
+        type='button'
+      >
+        <CheckIcon className='me-2' />
+        Update Order Status
+      </button>
+    </div>
+  );
+};
 
 /**
  * @function RenderContent
@@ -18,25 +60,37 @@ const RenderContent = ({orderData, orderStatus, onUpdateStatus, updateStatusActi
   }
 
   return (
-    <div>
-      <p>Order #: {orderData._id}</p>
-      <p><strong>Delivery Notes: </strong>{orderData.deliveryNotes}</p>
-      <p><strong>Order Total: </strong>${orderData.orderTotal}</p>
-      <select
-        onChange={e => onUpdateStatus({orderStatus: e.target.value})}
-        value={orderStatus}
-      >
-        <option value="">Select Status</option>
-        {ORDER_STATUSES.map(status => (
-          <option key={status.value} value={status.value}>
-            {status.label}
-          </option>
-        ))}
-      </select>
-      <button onClick={updateStatusAction}>
-        Update Order Status
-      </button>
-    </div>
+    <Form onSubmit={e => e.preventDefault()}>
+      <label className='mb-2' htmlFor='order-delivery-notes'>
+        <strong>Order #</strong>
+      </label>
+      <Form.Control
+        className='mb-3'
+        disabled
+        readOnly
+        value={orderData._id}
+      />
+      <label className='mb-2' htmlFor='order-delivery-notes'>
+        <strong>Delivery Notes</strong>
+      </label>
+      <Form.Control
+        className='mb-3'
+        disabled
+        name='order-delivery-notes'
+        readOnly
+        value={orderData.deliveryNotes}
+      />
+      <label className='mb-2' htmlFor='order-total'>
+        <strong>Order Total</strong>
+      </label>
+      <Form.Control
+        className='mb-3'
+        disabled
+        name='order-total'
+        readOnly
+        value={`${orderData.orderTotal}`}
+      />
+    </Form>
   );
 };
 
@@ -48,6 +102,7 @@ const RenderContent = ({orderData, orderStatus, onUpdateStatus, updateStatusActi
 export default function OrderDetailsPage() {
 
   const location = useLocation();
+  const navigate = useNavigate();
   const params = useParams();
   const [orderData, setOrderData] = useState();
   const [orderStatus, setOrderStatus] = useState('');
@@ -99,9 +154,20 @@ export default function OrderDetailsPage() {
   const contentProps = {orderData, orderStatus, onUpdateStatus, updateStatusAction}
 
   return (
-    <div>
-      <h2>Order Detail</h2>
+    <div className='p-2'>
+      <PageHeader
+        backButtonAction={() => navigate(-1)}
+        heading='Order Details'
+        subheading={`Order #: ${orderData ? orderData._id : '[Not Found]'}`}
+        actionsContainer={
+          <UpdateStatusControl
+            updateStatusAction={updateStatusAction}
+            onUpdateStatus={onUpdateStatus}
+            orderStatus={orderStatus}
+          />
+        }
+      />
       <RenderContent {...contentProps} />
     </div>
   );
-}
+};
